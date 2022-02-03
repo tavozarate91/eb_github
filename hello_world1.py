b692668d4f1f9c238b23c2ab9bf56a1f98a23e11
ldrojas005 <92893892+ldrojas005@users.noreply.github.com>
2022-02-03 22:11:34 +0100

    Update hello_world1.py

--- hello_world1.py
+++ hello_world1.py
@@ -2,3 +2,15 @@ print("Hello World 1")
 print("Hello WOrld 2")
 print("Hello Woorld 3")
 print("Hello World 4-gus")
+
+def f5()
+    return None
+
+def f6()
+    return None
+
+def f7() 
+    return None
+
+def f8()
+    return None
