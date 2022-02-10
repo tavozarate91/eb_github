41127c6076589ad4a264f76f632699ac002b8413
tavozarate91 <74512310+tavozarate91@users.noreply.github.com>
2022-02-10 13:37:08 +0100

    Empty hello_world1.py

--- hello_world1.py
+++ hello_world1.py
@@ -1,15 +1,1 @@
-#Camilo, please write f1() and f2()
-def f1(a):
-    return a * a
 
-def f2(a):
-    return a + a
-
-#Daniela, please write f3() and f4()
-def f3():
-    return 3
-
-def f4(x):
-    return x*x
-
-#Daniela, please write f5() and f6() 
