8e28abf70b19d48cb2654117a5078ce1a21cd54e
tavozarate91 <74512310+tavozarate91@users.noreply.github.com>
2022-02-12 00:57:33 +0100

    Revert "Adding f1() and f2()"

--- hello_world1.py
+++ hello_world1.py
@@ -1,8 +1,8 @@
 #ldrojas writes f1() and f2()
-def f1(a):
-    return a * a * 2
-
-def f2(a, b):
-    return a * b
 
 #camilomelo252 writes f3() and f4()
+def f3(a):
+    return a * a
+
+def f4(a):
+    return 2 * a
